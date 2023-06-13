792cf32b7ea88881dff45f2e1e5bf3575e2437b7
tianliangleng <2438001922@qq.com>
2023-06-13 14:22:28 +0800

    first commit

---
+++ src/ResponseFactory/Enum.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Phpcuisine\Bags\ResponseFactory;
+
+class Enum
+{
+
+    const DEFAULT = 0;
+    const OK = 1000;
+    const C_SUCCESS = 1001;
+    const R_SUCCESS = 1002;
+    const U_SUCCESS = 1003;
+    const D_SUCCESS = 1004;
+    const UP_SUCCESS = 1005;
+    const LOGIN_SUCCESS = 1006;
+    const ERROR = 2000;
+    const C_ERROR = 2001;
+    const R_ERROR = 2002;
+    const U_ERROR = 2003;
+    const D_ERROR = 2004;
+    const UP_ERROR = 2005;
+    const LOGIN_ERROR = 2006;
+
+    //定义状态码对应的消息体
+    const MSG = [];
+
+    //定义需要转换成default状态的状态码
+    const MSG_TO_SUCCESS = [];
+
+
+    //定义默认状态码对应的消息体
+    const DEFAULT_MSG = [
+        self::OK => "操作成功",
+        self::C_SUCCESS => "添加成功",
+        self::R_SUCCESS => "查询成功",
+        self::U_SUCCESS => "修改成功",
+        self::D_SUCCESS => "删除成功",
+        self::UP_SUCCESS => "上传成功",
+        self::LOGIN_SUCCESS => "登录成功",
+        self::ERROR => "操作失败",
+        self::C_ERROR => "添加失败",
+        self::R_ERROR => "查询失败",
+        self::U_ERROR => "修改失败",
+        self::D_ERROR => "删除失败",
+        self::UP_ERROR => "上传失败",
+        self::LOGIN_ERROR => "登录失败",
+    ];
+
+    protected function getEnumFromName(string $name): int
+    {
+
+        if ($name == "DEFAULT") {
+            return self::DEFAULT;
+        }
+
+        $code = constant("self::" . $name);
+
+        return $this->getEnum($code);
+    }
+
+
+    /**
+     * 获取状态码
+     */
+    protected function getEnum(int $code): int
+    {
+
+        //如果状态码小于2000或者设置了转换，则返回0
+        if ($code < 2000 || in_array($code, self::MSG_TO_SUCCESS)) {
+            return self::DEFAULT;
+        }
+
+        return $code;
+    }
+
+
+    /**
+     * 获取状态码对应的消息体
+     */
+    protected function getMsg(int $code): string
+    {
+
+        $msg = self::MSG;
+
+        if ($msg) {
+
+            $msg_key = array_keys($msg);
+            $d_msg_key = array_keys(self::DEFAULT_MSG);
+
+            if ($intersect = array_intersect($msg_key, $d_msg_key)) {
+                throw new \Exception("无法定义默认的状态码:" . implode("|", $intersect), 1);
+                return false;
+            }
+        }
+
+        $_msg = array_merge(self::DEFAULT_MSG, $msg);
+
+        return $_msg[$code];
+    }
+}
---
+++ src/ResponseFactory/Response.php
@@ -0,0 +1,233 @@
+<?php
+
+namespace Phpcuisine\Bags\ResponseFactory;
+
+class Response
+{
+
+    private $code;
+    private $msg;
+    private $data;
+    private $enum;
+
+    public function __construct()
+    {
+        $this->enum = new Enum();
+    }
+
+    public function __call($name, $arguments)
+    {
+
+        switch ($name) {
+
+            case 'success':
+                $this->_success($arguments);
+                break;
+            case 'error':
+                $this->_error($arguments);
+                break;
+            case 'list':
+                $this->_list($arguments);
+                break;
+        }
+
+        return $this->returnJson();
+    }
+
+
+    public function setEnum($enum)
+    {
+
+        $this->enum = $enum;
+
+        return $this;
+    }
+
+
+    /**
+     * 用于请求成功后的消息提醒
+     */
+    private function _success($arguments)
+    {
+
+        $count = count($arguments);
+
+        //如果只有一个参数
+        if ($count == 1) {
+
+
+            $data = $arguments[0];
+
+            $this->_getSuccess($data);
+
+            //判断该参数是不是状态码
+
+        } else if ($count == 2) {
+
+            list($first, $second) = $arguments;
+            //两个参数，则定义，第一个参数为返回的消息体，第二个参数为数据
+            $this->_getSuccess($first, $second);
+        } else {
+
+            throw new \Exception("success方法参数超出,不能大于两个参数", 1);
+
+            return false;
+        }
+    }
+
+
+    private function _getSuccess($first, $second = [])
+    {
+        if ($this->checkEnumCode($first)) {
+
+            //是状态码的话，
+            $this->code = $this->enum->getEnum($first);
+            $this->msg = $this->enum->getMsg($first);
+            $this->data = [
+                "message" => $this->msg,
+                "list" => $second
+            ];
+        } else {
+
+            $this->code = $this->enum->getEnumFromName("DEFAULT");
+            $this->msg = $this->enum->getMsg($this->enum->getEnumFromName("OK"));
+            $this->data = [
+                "message" => $first,
+                "list" => $second
+            ];
+        }
+    }
+
+
+    /**
+     * 用于请求失败后的消息提醒
+     */
+    private function _error($arguments)
+    {
+
+        $count = count($arguments);
+
+        //如果只有一个参数
+        if ($count == 1) {
+
+
+            $data = $arguments[0];
+
+            $this->_getError($data);
+
+            //判断该参数是不是状态码
+
+        } else if ($count == 2) {
+
+            list($first, $second) = $arguments;
+            //两个参数，则定义，第一个参数为返回的消息体，第二个参数为数据
+            $this->_getError($first, $second);
+        } else {
+
+            throw new \Exception("error方法参数超出,不能大于两个参数", 1);
+
+            return false;
+        }
+    }
+
+
+    private function _getError($first, $second = [])
+    {
+        if ($this->checkEnumCode($first)) {
+
+            //是状态码的话，
+            $this->code = $this->enum->getEnum($first);
+            $this->msg = $this->enum->getMsg($first);
+            $this->data = [
+                "message" => $this->msg,
+                "list" => $second
+            ];
+        } else {
+
+            $this->code = $this->enum->getEnumFromName("ERROR");
+            $this->msg = $this->enum->getMsg($this->enum->getEnumFromName("ERROR"));
+            $this->data = [
+                "message" => $first,
+                "list" => $second
+            ];
+        }
+    }
+
+
+    /**
+     * 用于输出数据
+     */
+    private function _list($arguments)
+    {
+
+        $count = count($arguments);
+
+        //如果只有一个参数
+        if ($count == 1) {
+
+
+            $data = $arguments[0];
+
+            $this->code = $this->enum->getEnum("DEFAULT");
+            $this->msg = $this->enum->getMsg($this->enum->getEnum("OK"));
+            $this->data = [
+                "message" => $this->msg,
+                "list" => $data
+            ];
+        } else if ($count == 2) {
+
+            list($first, $second) = $arguments;
+            //两个参数，则定义，第一个参数为状态码，第二个参数为数据
+            $this->_getList($first, $second);
+        } else {
+
+            throw new \Exception("list方法参数超出,不能大于两个参数", 1);
+
+            return false;
+        }
+    }
+
+
+    private function _getList($first, $second = [])
+    {
+        if ($this->checkEnumCode($first)) {
+
+            //是状态码的话，
+            $this->code = $this->enum->getEnum($first);
+            $this->msg = $this->enum->getMsg($first);
+            $this->data = [
+                "message" => $this->msg,
+                "list" => $second
+            ];
+        } else {
+
+            $this->code = $this->enum->getEnum("DEFAULT");
+            $this->msg = $this->enum->getMsg($this->enum->getEnumFromName("OK"));
+            $this->data = [
+                "message" => $first,
+                "list" => $second
+            ];
+        }
+    }
+
+    /**
+     * 判断是否是状态码
+     */
+    private function checkEnumCode($code)
+    {
+
+        return preg_match('/\d{4}/', $code);
+    }
+
+
+    private function returnJson()
+    {
+
+        return json_encode([
+            "code" => $this->code,
+            "msg" => $this->msg,
+            "data" => $this->data,
+            "time" => date("Y-m-d H:i:s")
+        ]);
+    }
+}
